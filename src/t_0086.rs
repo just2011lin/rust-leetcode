f6d8fe8429e731802c07a1256b0c6b985f9a2c66
just2011lin <chi_dou@qq.com>
2024-08-05 15:12:01 +0800

    86题 优化

--- src/t_0086.rs
+++ src/t_0086.rs
@@ -1,39 +1,26 @@
 // 86 合并两个有序数组
 
 pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-    let m = m as usize;
-    let n = n as usize;
+    let mut m = m as usize;
+    let mut n = n as usize;
     let len = (m + n) as usize;
-    let mut nums = vec![0; len];
-    let mut x = 0usize;
-    let mut y = 0;
-    loop {
-        if x + y >= len {
+
+    for i in 0..len {
+        let i = len - 1 - i;
+        if m == 0 {
+            nums1[i] = nums2[n - 1];
+            n -= 1;
+            continue;
+        }
+        if n == 0 {
             break;
         }
-        // 1中的值取完了
-        if x >= m {
-            nums[x + y] = nums2[y];
-            y += 1;
-            continue;
-        }
-        // 2中的值取完了
-        if y >= n {
-            nums[x + y] = nums1[x];
-            x += 1;
-            continue;
-        }
-        if nums1[x] <= nums2[y] {
-            nums[x + y] = nums1[x];
-            x += 1;
-            continue;
+        if nums2[n - 1] >= nums1[m - 1] {
+            nums1[i] = nums2[n - 1];
+            n -= 1;
         } else {
-            nums[x + y] = nums2[y];
-            y += 1;
-            continue;
+            nums1[i] = nums1[m - 1];
+            m -= 1;
         }
-    }
-    for i in 0..len {
-        nums1[i] = nums[i];
     }
 }
